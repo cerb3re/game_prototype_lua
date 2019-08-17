43d2919a797f5b1959cdaa419561846b8b6c58ee
Tanguy CHENIER <tanguy.chenier@gmail.com>
2019-08-17 13:57:55 +0200

    Map generation V2

--- Procedural_generation/main.lua
+++ Procedural_generation/main.lua
@@ -1,27 +1,59 @@
+-- Ecrit en FranGlais pour distinguer les deux actions
+-- Written in EngLench 
 io.stdout:setvbuf('no')
 if arg[#arg] == "-debug" then require("mobdebug").start() end
 
 love.graphics.setDefaultFilter("nearest")
+love.window.setTitle("Map Generation - T.CHENIER")
 
 local donjon = {}
 donjon.nombreDeColonnes = 9
 donjon.nombreDeLignes   = 6
 donjon.map              = {}
+donjon.salleDepart      = nil
 
-function generateMap()
+
+function createRoom(lign, column)
+  local newRoom = {}
+
+  newRoom.lign        = lign
+  newRoom.column      = column
+  newRoom.isOpen      = false
+  newRoom.doorHeight  = false
+  newRoom.doorRight   = false
+  newRoom.doorBotom   = false
+  newRoom.doorLeft    = false
+  
+  return newRoom
+end
+
+function generateDungeon()
   print("Map generation is loading...")
   
-  local nLigne, nColonne
+  local nLigne, nColonne, nLigneDepart, nColonneDepart
+  local listeSalles  = {}
+  local nbSalles    = 20
+  local startRoom
+  
+  nLigneDepart      = math.random(1, donjon.nombreDeLignes)
+  nColonneDepart    = math.random(1, donjon.nombreDeColonnes)
+  
   for nLigne = 1, donjon.nombreDeLignes, 1 do
     donjon.map[nLigne] = {}
     for nColonne = 1, donjon.nombreDeColonnes, 1 do
-      donjon.map[nLigne][nColonne] = nil
+      donjon.map[nLigne][nColonne] = createRoom(nLigne, nColonne)
     end
   end
+  
+  startRoom         = donjon.map[nLigneDepart][nColonneDepart]
+  startRoom.isOpen  = true
+  table.insert(listeSalles, startRoom)
+  
+  donjon.salleDepart = startRoom
 end
 
 function love.load()
-  generateMap()
+  generateDungeon()
 end
 
 function love.update(dt)
@@ -40,12 +72,16 @@ function love.draw()
     x = 5
     
     for nColonne = 1, donjon.nombreDeColonnes, 1 do
-      if donjon.map[nLigne][nColonne] == nil then
-        love.graphics.rectangle("fill", x, y, largeurCase, longeurCase)
+      if donjon.map[nLigne][nColonne].isOpen == false then
+        love.graphics.setColor(0.30, 0.30, 0.30)
+      else
+        love.graphics.setColor(255, 255, 255)
       end
+      love.graphics.rectangle("fill", x, y, largeurCase, longeurCase)
       
       x = x + largeurCase + espaceCase
     end
     y = y + longeurCase + espaceCase
   end
+  love.graphics.setColor(255, 255, 255)
 end
